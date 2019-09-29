a946ef3877068c4600630d797b5cf62b4720ce50
Muriel Silveira <murielkong@gmail.com>
2019-09-29 23:46:48 +0200

    feat: configure react router and add some sample routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,37 @@
 import React from 'react'
-import logo from './assets/logo.svg'
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom'
 import styles from './App.module.css'
+import Login from './apps/auth/pages/Login'
+import Dashboard from './apps/dashboard/pages/Dashboard'
+import logo from './assets/logo.svg'
 
 export default function App() {
   return (
-    <div className={styles.app}>
+    <Router>
       <header className={styles.header}>
-        <img src={logo} className={styles.logo} alt="logo" />
-        <span className={styles.title}>TS CRA</span>
+        <Link className={styles.logo} to="/">
+          <img className={styles.image} src={logo} alt="Logo" />
+          <span>TS CRA</span>
+        </Link>
         <nav className={styles.menu}>
           <ul>
-            <li>Dashboard</li>
-            <li>Elements</li>
-            <li>Login</li>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
           </ul>
         </nav>
       </header>
-    </div>
+      <Switch>
+        <Route path="/dashboard">
+          <Dashboard />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
