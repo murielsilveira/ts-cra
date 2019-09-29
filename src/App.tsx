0faca86ffbb9033072419b05818fcda08773abe1
Muriel Silveira <murielkong@gmail.com>
2019-09-29 23:29:05 +0200

    refactor: use css modules and change the styles to have a simple header

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
 import React from 'react'
 import logo from './assets/logo.svg'
-import './App.css'
+import styles from './App.module.css'
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={styles.app}>
+      <header className={styles.header}>
+        <img src={logo} className={styles.logo} alt="logo" />
+        <span className={styles.title}>TS CRA</span>
+        <nav className={styles.menu}>
+          <ul>
+            <li>Dashboard</li>
+            <li>Elements</li>
+            <li>Login</li>
+          </ul>
+        </nav>
       </header>
     </div>
   )
