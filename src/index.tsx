9da0d4532e03933c0e87fbfde1a8c4baad93c8a0
Muriel Silveira <murielkong@gmail.com>
2019-09-30 00:33:42 +0200

    feat: extract colors to css variables

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './styles/normalize.css'
+import './styles/variables.css'
 import './styles/index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
